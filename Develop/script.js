b5509c037cf6484e3cbe90f20dde00b129e3ca29
Ryan McCarthy <rmmcc92@aol.com>
2021-06-21 16:41:25 -0400

    Got the majority of the password choice code working. Need to add comments to reinforce what's going on and figure out what's going wrong with the join function as wthe array commas are being included in the final password.

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,185 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var pass = "";
+var pass = [];
 
-var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxy0123456789~!@#$%^&*'";
+var passLength;
+var upperPrompt;
+var lowerPrompt;
+var numberPrompt;
+var symbolPrompt;
+var finalPass=""
 
-var str2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxy0123456789";
-
-var specChar = prompt("Special Character? y/n");
-// Allows us to use the user input at the password length I though I would have to convert it from a string to a number but it seems to work this way
-var passLength = prompt("Password Length?");
+number = ["0","1" ,"2" ,"3" ,"4" ,"5" ,"6" ,"7" ,"8" ,"9"]
+symbol = ["~", "`", "!" , "@" , "#" , "$" , "%" , "^" , "&" , "*" ,"(" , ")" , "_" , "+" , "-" , "=" , "[" , "]" , "{" ,"}" , ";" ,":" ,"'" ,"<" ,">" ,"," ,"." , "/" , "?"]
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x" , "y" ,"z"];
+// Array will contain capitol letters
+capital = [];
+var choices = [];
+// converts letters to uppercase 
+var capitalize = function (x) {
+  return x.toUpperCase();
+};
+letters2 = letters.map(capitalize);
 
 
 // Write password to the #password input
-function generatePassword() {
-  // for loop is saying when this item is = 1 but < or equal to x increment by 1
-  // in other words while this generated number is under x keep incrementing by one and then stop when you hit 10
-  // this allows us to run the code for each letter in our password making it unique every time
-  for (i = 1; i <= passLength; i++) {
-// if the user types y it will pull from the string with special character if they type n it will pull from the string without special characters
-    if (specChar === "y"){ 
-// defines our variable of char randomly by using math functions
-    var char = Math.floor(Math.random() * str.length + 1);
-   pass += str.charAt(char);
-}
-   else if (specChar === "n") {
-    var char2 = Math.floor(Math.random() * str2.length +1 );
-    pass += str2.charAt(char2);
-  }
-// charAt returns the character at the specified location
-//+= is the sane as saying pass = pass + string
-// so by using these three arguments we are finding a random place in the string defined as char and then returning those values to the variable pass
- 
-  }
-  return pass
-}
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+function generatePassword() {
+  passLength = parseInt(prompt("Password Length?"));{
+
+    if(!passLength) {
+      window.alert("Please enter a value.");
+    }
+  
+   else if (passLength < 8 || passLength > 128) {
+    passLength= parseInt(prompt("Password must be between 8 and 128 characters."));
+  }
+  
+
+  else {
+    symbolPrompt = confirm("Special Character? y/n");
+    upperPrompt = confirm("Capitol Letters? y/n");
+    lowerPrompt = confirm("Lowercase letters? y/n");
+    numberPrompt = confirm("Numbers? y/n");
+  };
+
+console.log(passLength);
+console.log(symbolPrompt);
+console.log(upperPrompt);
+console.log(lowerPrompt);
+console.log(numberPrompt);
+
+
+
+  // None true
+
+  if (!upperPrompt && !lowerPrompt && !numberPrompt && !symbolPrompt) {
+    return window.alert("You must select at least one type of character.");
+  }
+
+  // All true
+  else if (upperPrompt && lowerPrompt && numberPrompt && symbolPrompt) {
+    choices += letters.concat(letters2, number, symbol);
+    
+  }
+
+  // Just symcheck false
+
+  else if (upperPrompt && lowerPrompt && numberPrompt && !symbolPrompt) {
+    choices += letters.concat(number, letters2);
+
+  }
+
+  // Just Numbercheck false
+
+  else if (upperPrompt && lowerPrompt && !numberPrompt && symbolPrompt) {
+    choices += letters.concat(letters2, symbol);
+    
+  }
+
+  // Just lowercheck false
+
+  else if (upperPrompt && !lowerPrompt && numberPrompt && symbolPrompt) {
+    choices += letters2.concat(number, symbol);
+    
+  }
+
+  // Just UperCheck false
+
+  else if (!upperPrompt && lowerPrompt && numberPrompt && symbolPrompt) {
+    choices += letters.concat(number, symbol);
+    
+  }
+
+  // Uppercheck and lowercheck false
+
+  else if (!upperPrompt && !lowerPrompt && numberPrompt && symbolPrompt) {
+    choices += number.concat(symbol);
+  }
+
+  // Uppercheck and numbercheck false
+
+  else if (!upperPrompt && lowerPrompt && !numberPrompt && symbolPrompt) {
+    choices += letters.concat(symbol);
+  }
+
+  // Uppercheck and symbolcheck false
+
+  else if (!upperPrompt && lowerPrompt && numberPrompt && !symbolPrompt) {
+    choices += letters.concat(number);
+  }
+
+  // lowercheck and numbercheck false
+
+  else if (upperPrompt && !lowerPrompt && numberPrompt && !symbolPrompt) {
+    choices += letters2.concat(symbol);
+  }
+
+  // lowercheck and symbolcheck false
+
+  else if (upperPrompt && !lowerPrompt && numberPrompt && !symbolPrompt) {
+    choices += letters2.concat(number);
+  }
+
+  // numbercheck and symbolcheck false
+
+  else if (upperPrompt && lowerPrompt && !numberPrompt && !symbolPrompt) {
+    choices += letters.concat(letters2);
+  }
+
+  // Uppercheck lowercheck and numbercheck false
+
+  else if (!upperPrompt && !lowerPrompt && !numberPrompt && symbolPrompt) {
+    choices += symbol;
+  }
+
+  // Uppercheck lowercheck and symbolcheck false
+
+  else if (!upperPrompt && !lowerPrompt && numberPrompt && !symbolPrompt) {
+    choices += number;
+  }
+
+  // Uppercheck numbercheck and symbolcheck false
+
+  else if (!upperPrompt && lowerPrompt && !numberPrompt && !symbolPrompt) {
+    choices += letters;
+  }
+
+  // lowercheck numbercheck and symbolcheck false
+
+  else if (upperPrompt && !lowerPrompt && !numberPrompt && !symbolPrompt) {
+    choices += capital.concat(letters2);
+  };
+
+  for (i = 0; i<=passLength; i++) {
+    var randomize = choices[Math.floor(Math.random() * choices.length)];
+    pass.push(randomize);
+  }
+
+  var finalPass = pass.join("");
+  Input(finalPass);
+  return finalPass;
+  }
+
+  function Input(finalPass){
+  document.getElementById("password").textContent= finalPass
+
+  window.alert(finalpass)
+
+}
+}
 
 /*
 GIVEN I need a new, secure password
