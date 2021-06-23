b357f2637b1456755ad1c8568504100c426f3551
Ryan McCarthy <rmmcc92@aol.com>
2021-06-23 15:03:43 -0400

    Cleaned up a few bits of unnecesary code and made the document slightly easier to read.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// These variables are left blank so that they can be filled by a later function
 var pass = [];
-
-var passLength;
-var upperPrompt;
-var lowerPrompt;
-var numberPrompt;
-var symbolPrompt;
 var finalPass = ""
 
 number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -168,24 +163,5 @@ function generatePassword() {
   // The password is sent to the textbox where the user can read it
   function Input(finalPass) {
     document.getElementById("password").textContent = finalPass
-
-
   }
 }
-
-/*
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page */
